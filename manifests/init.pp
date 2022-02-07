282f1041d09654fa679a5af91b381af345df647c
Ash Crosby <tcrosby@gmail.com>
2022-02-07 16:37:54 +0000

    argh

--- manifests/init.pp
+++ manifests/init.pp
@@ -92,8 +92,6 @@ class devpi (
   Optional[String] $ldap_group_filter,
   Optional[String] $ldap_group_attr,
 ) {
-  require python
-
   contain ::devpi::user
   contain ::devpi::package
   contain ::devpi::config
